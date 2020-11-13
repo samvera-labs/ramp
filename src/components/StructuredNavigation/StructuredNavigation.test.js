3858f66566816d8ad58e3de54dd82f04c4edff1c
Adam Arling <adam.arling@northwestern.edu>
2020-11-13 10:47:12 -0600

    Add a pattern for how to wrap tests in Providers to the StructuredNavigation component

--- src/components/StructuredNavigation/StructuredNavigation.test.js
+++ src/components/StructuredNavigation/StructuredNavigation.test.js
@@ -1,43 +1,55 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import StructuredNavigation from './StructuredNavigation';
 import manifest from '@Json/mahler-symphony-audio';
-import { ManifestProvider } from '../../context/manifest-context';
+import {
+  withManifestProvider,
+  withManifestAndPlayerProvider,
+  withPlayerProvider,
+} from '../../services/testing-helpers';
 
 describe('StructuredNavigation component', () => {
-  test.only('renders successfully', () => {
-    render(
-      <ManifestProvider initialState={{ manifest }}>
-        <StructuredNavigation manifest={manifest} />
-      </ManifestProvider>
-    );
-    expect(screen.getByTestId('structured-nav'));
-    screen.debug();
-  });
+  beforeEach(() => {
+    // An example of how we could pass props into
+    // the tested(in this case: StructuredNavigation) component directly
+    const props = {
+      foo: 'bar',
+    };
 
-  test('returns a List of items when structures are present in the manifest', async () => {
-    render(
-      <ManifestProvider initialState={{ manifest }}>
-        <StructuredNavigation manifest={manifest} />
-      </ManifestProvider>
-    );
-    await waitFor(() => {
-      expect(screen.getAllByTestId('list').length.toBeGreaterThan(0));
+    const NavWithPlayer = withPlayerProvider(StructuredNavigation, {
+      ...props,
     });
+    const NavWithManifest = withManifestProvider(NavWithPlayer, {
+      initialState: { manifest, canvasIndex: 0 },
+    });
+    render(<NavWithManifest />);
   });
 
-  test('returns message when structures are not present in manifest', () => {
+  test('renders successfully', () => {
+    expect(screen.getByTestId('structured-nav'));
+  });
+
+  test('returns a List of items when structures are present in the manifest', () => {
+    expect(screen.getAllByTestId('list').length).toBeGreaterThan(0);
+  });
+});
+
+describe('StructuredNavigation component without structures', () => {
+  test.only('renders no list items and a message when structures are not present in manifest', () => {
     let manifestWithoutStructures = JSON.parse(JSON.stringify(manifest));
     delete manifestWithoutStructures.structures;
 
-    render(
-      <ManifestProvider initialState={{ manifest: manifestWithoutStructures }}>
-        <StructuredNavigation manifest={manifestWithoutStructures} />
-      </ManifestProvider>
+    // Example of how to wrap tests with a "combo Provider" helper
+    const NavWithPlayerAndManifest = withManifestAndPlayerProvider(
+      StructuredNavigation,
+      {
+        initialManifestState: { manifest: manifestWithoutStructures },
+      }
     );
+    render(<NavWithPlayerAndManifest />);
+    screen.debug();
+
     expect(screen.queryByTestId('list')).toBeNull();
-    expect(
-      screen.getByText(/There are no structures in the manifest/)
-    ).toBeInTheDocument();
+    expect(screen.getByText(/There are no structures in the manifest/));
   });
 });
--- src/services/testing-helpers.js
+++ src/services/testing-helpers.js
@@ -1,33 +1,39 @@
 // counter.test.js
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
-import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
+import { ManifestProvider } from '../context/manifest-context';
+import { PlayerProvider } from '../context/player-context';
 
-// this is a handy function that I normally make available for all my tests
-// that deal with connected components.
-// you can provide initialState or the entire store that the ui is rendered with
-export function renderWithRedux(
-  ui,
-  {
-    initialState,
-    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
-  } = {},
-  renderFn = render
+export function withManifestAndPlayerProvider(
+  Component,
+  { initialManifestState = {}, initialPlayerState = {}, ...restProps } = {}
 ) {
-  const obj = {
-    ...renderFn(<Provider store={store}>{ui}</Provider>),
-    store,
-  };
-  obj.rerenderWithRedux = (el, nextState) => {
-    if (nextState) {
-      store.replaceReducer(() => nextState);
-      store.dispatch({ type: '__TEST_ACTION_REPLACE_STATE__' });
-      store.replaceReducer(reducer);
-    }
-    return renderWithRedux(el, { store }, obj.rerender);
-  };
-  return obj;
+  return () => (
+    <ManifestProvider initialState={initialManifestState}>
+      <PlayerProvider initialState={initialPlayerState}>
+        <Component {...restProps} />
+      </PlayerProvider>
+    </ManifestProvider>
+  );
+}
+
+export function withManifestProvider(
+  Component,
+  { initialState = {}, ...restProps } = {}
+) {
+  return () => (
+    <ManifestProvider initialState={initialState}>
+      <Component {...restProps} />
+    </ManifestProvider>
+  );
+}
+
+export function withPlayerProvider(
+  Component,
+  { initialState = {}, ...restProps } = {}
+) {
+  return () => (
+    <PlayerProvider initialState={initialState}>
+      <Component {...restProps} />
+    </PlayerProvider>
+  );
 }
