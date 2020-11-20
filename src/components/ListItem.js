4b0491c6e47bc46100b2a82be6a66c29b5d2688c
Phuong Dinh <phuongdh@gmail.com>
2020-11-20 12:16:09 -0500

    Indicate active structured navigation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import List from './List';
 import { getChildCanvases, getLabelValue } from '../services/iiif-parser';
 import PropTypes from 'prop-types';
 import { useManifestState } from '../context/manifest-context';
 import { usePlayerDispatch } from '../context/player-context';
+import { useManifestDispatch } from '../context/manifest-context';
 
 const ListItem = ({ item, isChild }) => {
-  const dispatch = usePlayerDispatch();
-  const manifestState = useManifestState();
+  const playerDispatch = usePlayerDispatch();
+  const manifestDispatch = useManifestDispatch();
+  const { manifest, currentNavItem } = useManifestState();
   const childCanvases = getChildCanvases({
     rangeId: item.id,
-    manifest: manifestState.manifest,
+    manifest: manifest,
   });
   const subMenu =
     item.items && item.items.length > 0 && childCanvases.length === 0 ? (
       <List items={item.items} isChild={true} />
     ) : null;
+  const liRef = useRef(null)
 
   const handleClick = (e) => {
     e.stopPropagation();
     e.preventDefault();
 
-    dispatch({ clickedUrl: e.target.href, type: 'navClick' });
+    playerDispatch({ clickedUrl: e.target.href, type: 'navClick' });
+    manifestDispatch({ item: e.target.href, type: 'switchItem' });
   };
 
   const renderListItem = () => {
@@ -39,8 +43,14 @@ const ListItem = ({ item, isChild }) => {
     return null;
   };
 
+  useEffect(() => {
+    if (currentNavItem == item.items[0].id) {
+      liRef.current.className += " active";
+    }
+  }, [currentNavItem]);
+
   return (
-    <li data-testid="list-item" className="irmp--structured-nav__list-item">
+    <li data-testid="list-item" ref={liRef} className="irmp--structured-nav__list-item">
       {renderListItem()}
       {subMenu}
     </li>
