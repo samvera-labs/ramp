a8dfcdb9ffd86d92879a1cafec59cc0be902185c
dananji <dwithana@iu.edu>
2023-01-06 16:21:19 -0500

    Cleanup code and files

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import config from '../env';
-
-
+import config from './config';
 
 ReactDOM.render(<App
   manifestURL={`${config.url}/${config.env}/lunchroom_manners.json`}
--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 let config = {};
-console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'development') {
-  config.url = 'http://localhost:3000';
-  config.env = 'dev';
+  config.url = 'http://localhost:6060';
+  config.env = 'development';
 } else {
-  config.url = 'https://iiif-react-media-player.netlify.app';
-  config.env = 'prod';
+  config.url = 'https://samvera-labs.github.io/iiif-react-media-player';
+  config.env = 'production';
 }
 
 export default config;
