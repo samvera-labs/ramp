050a38050d13a0bf10049dee41aa3b0f5aa6b5b0
dananji <dwithana@iu.edu>
2023-06-14 17:37:43 -0400

    Add Styleguidist docs for MediaPlayer component

--- src/components/MediaPlayer/VideoJS/components/js/VideoJSCurrentTime.js
+++ src/components/MediaPlayer/VideoJS/components/js/VideoJSCurrentTime.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import videojs from 'video.js';
-import { timeToHHmmss } from '../../../../services/utility-helpers';
+import { timeToHHmmss } from '@Services/utility-helpers';
 
 const vjsComponent = videojs.getComponent('Component');
 
