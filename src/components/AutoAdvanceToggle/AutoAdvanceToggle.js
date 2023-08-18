6da5a674fbb31aa30efcd1a317bb2dbce9a66e2b
Chris Colvard <chris.colvard@gmail.com>
2023-08-18 16:16:07 -0400

    Add top level auto-advance toggle component.
    Co-authored-by: Mason Ballengee <masaball@iu.edu>

---
+++ src/components/AutoAdvanceToggle/AutoAdvanceToggle.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useManifestState } from '../../context/manifest-context';
+import { parseManifest } from 'manifesto.js';
+import './AutoAdvanceToggle.scss';
+
+const AutoAdvanceToggle = ({ label = "Autoplay", showLabel = true }) => {
+  const { manifest } = useManifestState();
+
+  const [autoAdvance, setAutoAdvance] = React.useState();
+
+  React.useEffect(() => {
+    if (manifest) {
+      //Parse manifest to see if auto_advance behavior present at manifest level then save into state
+      const manifestParsed = parseManifest(manifest);
+      console.log(manifestParsed.getBehavior());
+      const autoAdvanceBehavior = manifestParsed.getBehavior() === "auto-advance";
+      setAutoAdvance(autoAdvanceBehavior);
+    }
+  }, [manifest]);
+
+  return (
+    <div data-testid="auto-advance" className="ramp--auto-advance">
+      {showLabel && (
+        <span className="ramp--auto-advance-label" data-testid="auto-advance-label">{label}</span>
+      )}
+      <input type="checkbox" data-toggle="toggle" data-size="mini" name="ramp--auto-advance-toggle" checked={autoAdvance} onChange={e => setAutoAdvance(e.target.checked)} />
+    </div>
+  );
+};
+
+export default AutoAdvanceToggle;
--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ export { default as StructuredNavigation } from '@Components/StructuredNavigatio
 export { default as Transcript } from '@Components/Transcript/Transcript';
 export { default as MetadataDisplay } from '@Components/MetadataDisplay/MetadataDisplay';
 export { default as SupplmentalFiles } from '@Components/SupplementalFiles/SupplementalFiles';
+export { default as AutoAdvanceToggle } from '@Components/AutoAdvanceToggle/AutoAdvanceToggle';
