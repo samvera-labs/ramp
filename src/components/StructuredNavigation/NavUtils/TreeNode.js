fd6b60372c13acfe78504cdfc89f6807ecc59395
dwithana <dwithana@iu.edu>
2025-03-19 17:15:48 -0400

    Add keyboard navigation support and fix CSS for HTML changes

--- src/components/StructuredNavigation/NavUtils/TreeNode.js
+++ src/components/StructuredNavigation/NavUtils/TreeNode.js
@@ -1,9 +1,33 @@
 import React, { Fragment, useEffect, useMemo, useRef, useState } from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 import { autoScroll, CANVAS_MESSAGE_TIMEOUT } from '@Services/utility-helpers';
 import { LockedSVGIcon } from '@Services/svg-icons';
 import { useActiveStructure, useCollapseExpandAll } from '@Services/ramp-hooks';
 
+/**
+ * Build leaf-level nodes in the structures in Manifest. These nodes can be
+ * either timespans (with media fragment) or titles (w/o media fragment).
+ * @param {Object} props
+ * @param {Number} props.canvasDuration duration of the Canvas associated with the item
+ * @param {Number} props.canvasIndex index of the Canvas associated with the item
+ * @param {Number} props.duration duration of the item
+ * @param {String} props.id media fragemnt of the item
+ * @param {Boolean} props.isTitle flag to indicate item w/o mediafragment
+ * @param {Boolean} props.isCanvas flag to indicate item is at Canvas-level
+ * @param {Boolean} props.isClickable flag to indicate item is within resource duration
+ * @param {Boolean} props.isEmpty flag to indicate Canvas associated with item is inaccessible
+ * @param {String} props.label text label of the item
+ * @param {String} props.summary summary associated with the item (in playlist context)
+ * @param {String} props.homepage homepage associated with the item (in playlist context)
+ * @param {String} props.isRoot root level node for structure
+ * @param {Array} props.items list of children for the item
+ * @param {Number} props.itemIndex index of the item within the section/canvas
+ * @param {String} props.rangeId unique id of the item
+ * @param {Number} props.sectionCount total number of sections in structure
+ * @param {Object} props.sectionRef React ref of the section element associated with the item
+ * @param {Object} props.structureContainerRef React ref of the structure container
+ */
 const TreeNode = ({
   canvasDuration,
   canvasIndex,
@@ -30,13 +54,10 @@ const TreeNode = ({
   // Root structure items are always expanded
   const [sectionIsCollapsed, setSectionIsCollapsed] = useState(isRoot ? false : true);
 
-  // Identify item as a SectionHeading for canvases in non-playlist contexts
-  const isSectionHeading = useMemo(() => { return isCanvas && !isPlaylist; }, [isCanvas, isPlaylist]);
-  const hasChildren = useMemo(() => { return items?.length > 0; }, [items]);
-
   const { currentNavItem, handleClick, isActiveLi,
     isActiveSection, isPlaylist, isPlaying, screenReaderTime } = useActiveStructure({
       itemId: id,
+      itemIndex,
       liRef: isSectionHeading ? sectionRef : liRef,
       sectionRef,
       structureContainerRef,
@@ -45,6 +66,10 @@ const TreeNode = ({
       canvasDuration,
       setSectionIsCollapsed
     });
+
+  // Identify item as a SectionHeading for canvases in non-playlist contexts
+  const isSectionHeading = useMemo(() => { return isCanvas && !isPlaylist; }, [isCanvas, isPlaylist]);
+  const hasChildren = useMemo(() => { return items?.length > 0; }, [items]);
 
   /*
     Auto-scroll active structure item into view only when user is not actively
@@ -62,6 +87,33 @@ const TreeNode = ({
       liRef.current.isClicked = false;
     }
   }, [currentNavItem]);
+
+  // Collapse/Expand section when all sections are collapsed/expanded respectively
+  useEffect(() => {
+    // Do nothing for root structure items
+    if (!isRoot) setSectionIsCollapsed(isCollapsed);
+  }, [isCollapsed]);
+
+  /*
+    Auto-scroll active section into view only when user is not
+    actively interacting with structured navigation
+  */
+  useEffect(() => {
+    if (canvasIndex + 1 === itemIndex && sectionRef.current
+      && sectionRef.current.isClicked != undefined && !sectionRef.current.isClicked
+      && structureContainerRef.current.isScrolling != undefined
+      && !structureContainerRef.current.isScrolling
+      && isSectionHeading) {
+      autoScroll(sectionRef.current, structureContainerRef);
+    }
+    if (sectionRef.current) sectionRef.current.isClicked = false;
+  }, [canvasIndex, isSectionHeading]);
+
+  useEffect(() => {
+    if (isPlaying && isActiveSection) {
+      autoScroll(sectionRef.current, structureContainerRef);
+    }
+  }, [isPlaying, isActiveSection]);
 
   // Build aria-label based on the structure item and context
   const ariaLabel = useMemo(() => {
@@ -93,42 +145,26 @@ const TreeNode = ({
   // Handle click on section heading button
   const handleSectionClick = (e) => {
     handleClick(e);
-    if (isActiveSection) {
-      toggleOpen(e);
-    }
+    if (isActiveSection) toggleOpen();
   };
 
+  /**
+   * Handle keydown event when focused on a clickable section item
+   * @param {Event} e 
+   */
   const handleSectionKeyDown = (e) => {
-    if (e.key === 'Enter' || e.key === 'Space') {
-      handleSectionClick(e);
+    // Do nothing when focused on a none time-synced item, e.g.: section without a mediafragment
+    if (id === undefined) return;
+    // Expand section and update player for keypresses on Enter/Space/ArrowRight keys
+    if (e.keyCode === 13 || e.keyCode === 32 || e.keyCode === 39) {
+      handleClick(e);
+      // Only toggle collapsible section is it's collapsed
+      if (sectionIsCollapsed) toggleOpen();
+    } else if (e.keyCode === 37 && !sectionIsCollapsed) {
+      // If the section is expanded, toggle it when ArrowLeft key is pressed
+      toggleOpen();
     }
   };
-
-  // Collapse/Expand section when all sections are collapsed/expanded respectively
-  useEffect(() => {
-    // Do nothing for root structure items
-    if (!isRoot) setSectionIsCollapsed(isCollapsed);
-  }, [isCollapsed]);
-
-  /*
-    Auto-scroll active section into view only when user is not
-    actively interacting with structured navigation
-  */
-  useEffect(() => {
-    if (canvasIndex + 1 === itemIndex && sectionRef.current
-      && sectionRef.current.isClicked != undefined && !sectionRef.current.isClicked
-      && structureContainerRef.current.isScrolling != undefined
-      && !structureContainerRef.current.isScrolling) {
-      autoScroll(sectionRef.current, structureContainerRef);
-    }
-    sectionRef.current.isClicked = false;
-  }, [canvasIndex]);
-
-  useEffect(() => {
-    if (isPlaying && isActiveSection) {
-      autoScroll(sectionRef.current, structureContainerRef);
-    }
-  }, [isPlaying]);
 
   const collapsibleButton = () => {
     return (
@@ -148,9 +184,14 @@ const TreeNode = ({
       <Fragment key={rangeId}>
         {isSectionHeading // Render items as SectionHeadings in non-playlist contexts
           ? (
-            <div className='ramp--structured-nav__section ramp--structured-nav__section-head-buttons'
+            <div className={cx(
+              'ramp--structured-nav__section',
+              'ramp--structured-nav__section-head-buttons',
+              isActiveSection ? 'active' : ''
+            )}
               data-mediafrag={id ?? ''}
-              tabIndex={-1}>
+              tabIndex={-1}
+            >
               <button
                 data-testid={id == undefined ? 'listitem-section-span' : 'listitem-section-button'}
                 ref={sectionRef}
@@ -163,6 +204,7 @@ const TreeNode = ({
                   !id && 'not-clickable',
                   isActiveSection ? 'active' : ''
                 )}
+                tabIndex={-1}
               >
                 <span className='ramp--structured-nav__title' aria-label={label}>
                   {isRoot ? '' : `${itemIndex}.`}
@@ -213,16 +255,6 @@ const TreeNode = ({
     );
   };
 
-
-  const handleKeyDown = (e) => {
-    e.preventDefault();
-    console.log('Item: ', label, ' on focus');
-    if (e.key === 'Enter' || e.key === 'Space') {
-      handleClick(e);
-      // Click section/timespan
-    }
-  };
-
   if (label != '') {
     return (
       <li
@@ -237,11 +269,9 @@ const TreeNode = ({
         data-label={label}
         data-summary={summary}
         aria-expanded={items?.length > 0 ? 'true' : undefined}
-        tabIndex={-1}
-        onKeyDown={handleKeyDown}
       >
         {renderListItem()}
-        {(!sectionIsCollapsed || hasChildren) && (
+        {((!sectionIsCollapsed && hasChildren) || isTitle) && (
           <ul className='ramp--structured-nav__list' role='group'>
             {items.map((item, index) => {
               return (
@@ -259,6 +289,27 @@ const TreeNode = ({
       </li>
     );
   }
+};
+
+TreeNode.propTypes = {
+  canvasDuration: PropTypes.number.isRequired,
+  canvasIndex: PropTypes.number.isRequired,
+  duration: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  isTitle: PropTypes.bool.isRequired,
+  isCanvas: PropTypes.bool.isRequired,
+  isClickable: PropTypes.bool.isRequired,
+  isEmpty: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  summary: PropTypes.string,
+  homepage: PropTypes.string,
+  isRoot: PropTypes.bool.isRequired,
+  items: PropTypes.array.isRequired,
+  itemIndex: PropTypes.number,
+  rangeId: PropTypes.string.isRequired,
+  sectionCount: PropTypes.number.isRequired,
+  sectionRef: PropTypes.object.isRequired,
+  structureContainerRef: PropTypes.object.isRequired
 };
 
 export default TreeNode;
