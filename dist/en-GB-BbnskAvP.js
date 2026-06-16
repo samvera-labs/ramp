d26973ea441cf8ac0d5edec4e1c856a3bfe0dc80
dwithana <dwithana@iu.edu>
2026-06-16 18:36:17 +0000

    Auto-build from main (70ea545) [ci skip]

---
+++ dist/en-GB-BbnskAvP.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const o="Colour",e={Color:o};exports.Color=o;exports.default=e;
---
+++ dist/en-GB-CiIpNUmc.js
@@ -0,0 +1,7 @@
+const o = "Colour", t = {
+  Color: o
+};
+export {
+  o as Color,
+  t as default
+};
