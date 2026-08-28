f308a9d49b5164cb78f3e7b60b1c08cc427b8394
dwithana <dwithana@iu.edu>
2026-08-28 15:42:08 -0700

    Remove durationchange override for mult-source Canvas

--- src/components/MediaPlayer/VideoJS/VideoJSPlayer.js
+++ src/components/MediaPlayer/VideoJS/VideoJSPlayer.js
@@ -206,15 +206,6 @@ function VideoJSPlayer({
       });
     });
 
-    /* VHS can re-report an individual source's duration via a 'durationchange' event
-    after the initial play/seek events. This silently over-writes the Canvas duration
-    set by Ramp. This is especially required for multi-source Canvases. */
-    player.on('durationchange', () => {
-      if (canvasDurationRef.current && player.duration() !== canvasDurationRef.current) {
-        player.duration(canvasDurationRef.current);
-      }
-    });
-
     player.on('ready', function () {
       console.log('Player ready');
 
